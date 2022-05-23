2f64d8c709949b7052e76cd8c46f265e27a5f5c3
brkckr20 <burak2043@gmail.com>
2022-05-23 10:13:02 +0300

    uygulama acildiginda mesajlarin listelenmesi islemi yapildi

--- chat-app/src/components/Container.js
+++ chat-app/src/components/Container.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react'
 import ChatForm from './ChatForm'
 import ChatList from './ChatList'
-import { init, subscribeChat } from '../socketApi'
+import { init, subscribeChat, subscribeInitialMessages } from '../socketApi'
 import { useChat } from '../contexts/ChatContext'
 
 const Container = () => {
     const { setMessages } = useChat();
     useEffect(() => {
         init();
+
+        subscribeInitialMessages((messages) => setMessages(messages))
 
         subscribeChat((message) => {
             setMessages((prevState) => [...prevState, { message }]);
