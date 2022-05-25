33bbb5585f856a9e9fc0f8c751f9ea2997b96d6a
brkckr20 <burak2043@gmail.com>
2022-05-25 16:47:19 +0300

    custom package ve deploymentta surge.sh deployment eklendi

---
+++ deploy/surge.sh/not.js
@@ -0,0 +1,9 @@
+/*
+ npm install --global surge ile paket yüklenir.
+ ilgili index.html in oldupu sayfadaki komut satırında surge komutu çalıştırılır.
+ yönergeler izlenir.
+
+ güncellemeler için
+    ilgili domain yerine daha önceden verilmiş olan domain yapıstırılır ve islemlere devam edilir.
+
+*/
